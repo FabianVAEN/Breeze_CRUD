c62fd3dc3965fc6bed3e56e0f5925e2cb29023d4
Fabian Vega <favegae@puce.edu.ec>
2025-12-15 09:55:16 -0500

    Feature: Usserseeders | created and aplicated

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User; // Importa el modelo de usuario
+use Illuminate\Support\Facades\Hash; // Para la encriptación de la contraseña
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 1. Crear Usuario Principal (Cuenta conocida)
+        User::create([
+            'name' => 'Admin Test',
+            'email' => 'admin@test.com', // Una cuenta que siempre conozcas
+            'password' => Hash::make('password'),
+        ]);
+
+        // 2. Crear 49 Usuarios de Prueba con Factory (Total 50)        
+        // El método factory() usa el UserFactory.php para generar datos ficticios.
+        User::factory()->count(49)->create(); 
+        
+    }
+}
